fdf019b412574cf05947fceb879068ede034e20f
Cin Noguera <ciin.noguera.gmail.com>
2024-11-20 15:53:07 -0300

    TypeOrm Config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,33 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { GoogleModule } from './google/google.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import typeOrmConfig from './config/typeorm'
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [AuthModule, UserModule, GoogleModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeOrmConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => 
+        configService.get('typeorm'),
+    }),
+    AuthModule, 
+    UserModule, 
+    GoogleModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_KEY_SECRET,
+      signOptions: {
+        expiresIn: '1h',
+      },
+    }),
+  ],
   controllers: [],
   providers: [],
 })
