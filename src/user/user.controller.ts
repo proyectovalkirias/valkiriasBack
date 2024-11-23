1864a52b5edd880a0758e92431a6a9366c57d72b
Cin Noguera <ciin.noguera.gmail.com>
2024-11-23 01:25:05 -0300

    Primeros endpoint de Users

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Put } from '@nestjs/common';
+import { UserService } from './user.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { User } from 'src/entities/user.entity';
+import { UserDto } from 'src/dtos/userDto';
 
-@Controller('user')
-export class UserController {}
+
+@ApiTags('users')
+@Controller('users')
+export class UserController {
+    constructor( private readonly userService: UserService){}
+
+    @ApiOperation({ summary: 'Get All Users' })
+    @Get()
+    getAllUser(){
+        return this.userService.getAllUser();
+    }
+
+    @ApiOperation({summary: 'Get User By Email'})
+    @Get(':email')
+    getUserByEmail(@Param('email') email: string){
+        return this.userService.getUserByEmail(email);
+    }
+
+
+    @ApiOperation({ summary:'Get User By Id'})
+    @Get(':id')
+    getUserById(@Param('id') id: string){
+        return this.userService.getUserById(id)
+    }
+
+    @ApiOperation({summary: 'Update User'})
+    @Put(':id')
+    udpdateUser(@Param('id') id: string, @Body() updateUser: UserDto){
+        return this.userService.updateUser(id, updateUser);
+    }
+}
--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -10,7 +10,24 @@ export class UserRepository {
         private readonly userRepository: Repository<User>
     ){}
 
-    getAllUser() {
+    getAllUser(): Promise<User[]> {
         return this.userRepository.find();
     }
-}
+
+    getUserById(userId: string): Promise <User>{
+        return this.userRepository.findOne({where: {id: userId}})
+    }
+
+    getUserByEmail(email: string): Promise<User | null>{
+        return this.userRepository.findOne({where: {email: email}})
+    }
+
+    createUser(userData: Partial<User>): Promise<User>{
+        const newUser = this.userRepository.create(userData);
+        return this.userRepository.save(newUser);
+    }
+
+    userUpdate(id: string, updateUser: Partial<User>){
+        return this.userRepository.update(id, updateUser);
+    }
+} 
