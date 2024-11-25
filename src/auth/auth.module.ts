bf710b5cf7cc17ea70744d624a4f5cf2b04d9805
Cin Noguera <ciin.noguera.gmail.com>
2024-11-25 19:12:46 -0300

    Elimine carpeta duplicada

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UserRepository } from 'src/user/user.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/entities/user.entity';
 
 @Module({
-  providers: [AuthService],
+  imports: [
+    TypeOrmModule.forFeature([User])
+  ],
+  providers: [AuthService, UserRepository],
   controllers: [AuthController]
 })
 export class AuthModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './config/swaggerConfig';
-import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.use(cors());
   
   setupSwagger(app);
   
