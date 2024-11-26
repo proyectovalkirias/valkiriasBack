29dacd70765a50eb0c9e9be626dff3838979febf
Cin Noguera <ciin.noguera.gmail.com>
2024-11-26 12:30:50 -0300

    Google Strategy

--- src/google/google.module.ts
+++ src/google/google.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { GoogleService } from './google.service';
+import { GoogleController } from './google.controller';
 
-@Module({})
+@Module({
+  providers: [GoogleService],
+  controllers: [GoogleController]
+})
 export class GoogleModule {}
