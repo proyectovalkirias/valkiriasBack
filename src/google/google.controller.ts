7194e0170f0f8773cb64a86274682817bbe11023
Cin Noguera <ciin.noguera.gmail.com>
2024-11-29 15:27:37 -0300

    Google

--- src/google/google.controller.ts
+++ src/google/google.controller.ts
@@ -2,41 +2,114 @@ import {
   BadRequestException,
   Controller,
   Get,
-  NotFoundException,
-  Req,
+  HttpStatus,
+  Query,
   Res,
-  UseGuards,
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { GoogleService } from './google.service';
-import { AuthGuard } from '@nestjs/passport';
 import { Response } from 'express';
 
 @ApiTags('google')
 @Controller('google')
 export class GoogleController {
-  constructor(private readonly googleService: GoogleService) {}
-
-  @Get()
-  @UseGuards(AuthGuard('google'))
-  googleLogin() {
-    return 'Redirecting to Google...';
-  }
+  constructor(
+    private readonly googleService: GoogleService,
+  ) {}
 
   @Get('redirect')
-  @UseGuards(AuthGuard('google'))
-  async googleRedirect(@Req() req, @Res() res: Response) {
-    const user = req.user;
+  async handleGoogleAuth(@Query('code') code: string, @Res() res: Response) {
+    console.log("Received code:", code);
+    try {
+      // Verifica si el código está presente en la URL
+      if (!code) {
+        throw new BadRequestException('Authorization code is required');
+      }
 
-    console.log('User after Google authentication:', req.user);
+      // Obtiene el token de Google usando el servicio
+      const accessToken = await this.googleService.getGoogleAuthToken(code);
+      console.log('Received access token:', accessToken);
+      
+      // Obtiene la información del usuario con el token
+      const userInfo = await this.googleService.getUserInfo(accessToken);
 
-    if (!user) {
-      throw new BadRequestException('Google autentication failed');
+      res.status(HttpStatus.OK).json({
+        user: userInfo,
+        token: accessToken, 
+      });
+    } catch (error) {
+      console.error('Google Auth error:', error);
+      res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        message: 'Error handling Google Auth',
+        error: error.message,
+      });
     }
-
-    const loginResponse = await this.googleService.googleLogin(user)
-
-
-     return res.status(200).json(loginResponse);  
   }
 }
+
+
+
+  // @Post()
+  // async handleGoogleAuth(@Body('code') code: string, @Res() res: Response) {
+  //   try {
+  //     if (!code) {
+  //       throw new HttpException('Missing Google authorization code', HttpStatus.BAD_REQUEST);
+  //     }
+
+  //     const tokenResponse = await axios.post(
+  //       'https://oauth2.googleapis.com/token',
+  //       {
+  //         code,
+  //         client_id: process.env.GOOGLE_CLIENT_ID,
+  //         client_secret: process.env.GOOGLE_CLIENT_SECRET,
+  //         redirect_uri: 'http://localhost:3001/Landing',
+  //         grant_type: 'authorization_code',
+  //       }
+  //     );
+
+  //     const userInfoResponse = await axios.get(
+  //       'https://www.googleapis.com/oauth2/v1/userinfo',
+  //       {
+  //         headers: {
+  //           Authorization: `Bearer ${tokenResponse.data.access_token}`,
+  //         },
+  //       }
+  //     );
+
+  //     res.status(HttpStatus.OK).json(userInfoResponse.data);
+  //   } catch (error) {
+  //     console.error(error);
+  //     res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+  //       message: 'Error handling Google Auth',
+  //       error: error.message,
+  //     });
+  //   }
+  // }
+
+
+  
+  
+
+  // @Get()
+  // @UseGuards(AuthGuard('google'))
+  // googleLogin() {
+  //   return 'Redirecting to Google...';
+  // }
+
+  // @Get('redirect')
+  // @UseGuards(AuthGuard('google'))
+  // async googleRedirect(@Req() req, @Res() res: Response) {
+  //   const user = req.user;
+
+  //   console.log('User after Google authentication:', req.user);
+
+  //   if (!user) {
+  //     throw new BadRequestException('Google autentication failed');
+  //   }
+
+  //   const loginResponse = await this.googleService.googleLogin(user)
+
+
+  //    return res.status(200).json(loginResponse);  
+  // }
+
--- src/google/google.strategy.ts
+++ src/google/google.strategy.ts
@@ -1,49 +1,87 @@
-import { Injectable } from '@nestjs/common';
-import { PassportStrategy } from '@nestjs/passport';
-import { Strategy, VerifyCallback } from 'passport-google-oauth20';
+import { BadGatewayException, BadRequestException, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import axios from 'axios';
 
 @Injectable()
-export class GoogleStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly configService: ConfigService) {
-    super({
-      clientID: configService.get<string>('GOOGLE_CLIENT_ID'),
-      clientSecret: configService.get<string>('GOOGLE_CLIENT_SECRET'),
-      callbackURL: configService.get<string>('GOOGLE_CALLBACK_URL'),
-      scope: ['email', 'profile'],
-    });
-  }
+export class GoogleStrategy {
+  constructor(private readonly configService: ConfigService) {}
 
-  async validate(
-    accessToken: string,
-    refreshToken: string,
-    profile: any,
-    done: (error: any, user?: any) => void,
-  ): Promise<any> {
-    console.log('Google profile:', profile);
-    console.log('Access token:', accessToken);
-    try {
-      if(!profile){
-        return 'Perfil no encontrado'
+
+    async getGoogleToken(authCode: string): Promise<any> {
+ 
+      const clientID = this.configService.get<string>('GOOGLE_CLIENT_ID');
+      const clientSecret = this.configService.get<string>('GOOGLE_CLIENT_SECRET');
+      const callbackURL = this.configService.get<string>('GOOGLE_CALLBACK_URL');
+
+      try {
+        const { data } = await axios.post('https://oauth2.googleapis.com/token', {
+          code: authCode,
+          client_id: clientID,
+          client_secret: clientSecret,
+          redirect_uri: callbackURL,
+          grant_type: 'authorization_code'
+        });
+
+        return data;
+      } catch (error) {
+        console.error('Error al obtener token de Google', error);
+        throw new BadRequestException('Error obteniendo el token de Google');
       }
-        
-      const { name, emails, photos } = profile;
-  
-      const user = {
-        email: emails[0].value,
-        firstname: name?.givenName,
-        lastname: name?.familyName,
-        photo: photos?.[0].value,
-        accessToken,
-      };
-  
-      console.log('Datos de usuario:', user);
-  
-      done(null, user);
-    } catch (error) {
-      console.error('Error en GoogleStrategy validate:', error);
-      done(error, null);
     }
+    
+          async getGoogleProfile(access_token: string): Promise<any> {
+            try {
+              const { data } = await axios.get('https://www.googleapis.com/oauth2/v3/userinfo', {
+                headers:{
+                  Authorization: `Bearer ${access_token}`
+                },
+              });
+    
+              return data;
+            } catch (error) {
+              console.error('Error obteniendo el perfin de Google:', error);
+              throw new BadGatewayException('Error obteniendo el perfil de Google');
+            }
+
+    }
+  //   super({
+  //     clientID: configService.get<string>('GOOGLE_CLIENT_ID'),
+  //     clientSecret: configService.get<string>('GOOGLE_CLIENT_SECRET'),
+  //     callbackURL: configService.get<string>('GOOGLE_CALLBACK_URL'),
+  //     scope: ['email', 'profile'],
+  //   });
+  // }
+
+  // async validate(
+  //   accessToken: string,
+  //   refreshToken: string,
+  //   profile: any,
+  //   done: (error: any, user?: any) => void,
+  // ): Promise<any> {
+  //   console.log('Google profile:', profile);
+  //   console.log('Access token:', accessToken);
+  //   try {
+  //     if(!profile){
+  //       return 'Perfil no encontrado'
+  //     }
+        
+  //     const { name, emails, photos } = profile;
+  
+  //     const user = {
+  //       email: emails[0].value,
+  //       firstname: name?.givenName,
+  //       lastname: name?.familyName,
+  //       photo: photos?.[0].value,
+  //       accessToken,
+  //     };
+  
+  //     console.log('Datos de usuario:', user);
+  
+  //     done(null, user);
+  //   } catch (error) {
+  //     console.error('Error en GoogleStrategy validate:', error);
+  //     done(error, null)
+  //   }
 
   }
-}
+
