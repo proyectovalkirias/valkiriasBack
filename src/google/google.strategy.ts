2537f5713bfd42d6e72eb43dae8564b169e2dc6f
Cin Noguera <ciin.noguera.gmail.com>
2024-12-02 12:57:22 -0300

    Google al fin

--- src/google/google.strategy.ts
+++ src/google/google.strategy.ts
@@ -48,42 +48,4 @@ export class GoogleStrategy {
       throw new BadGatewayException('Error obteniendo el perfil de Google');
     }
   }
-  //   super({
-  //     clientID: configService.get<string>('GOOGLE_CLIENT_ID'),
-  //     clientSecret: configService.get<string>('GOOGLE_CLIENT_SECRET'),
-  //     callbackURL: configService.get<string>('GOOGLE_CALLBACK_URL'),
-  //     scope: ['email', 'profile'],
-  //   });
-  // }
-
-  // async validate(
-  //   accessToken: string,
-  //   refreshToken: string,
-  //   profile: any,
-  //   done: (error: any, user?: any) => void,
-  // ): Promise<any> {
-  //   console.log('Google profile:', profile);
-  //   console.log('Access token:', accessToken);
-  //   try {
-  //     if(!profile){
-  //       return 'Perfil no encontrado'
-  //     }
-
-  //     const { name, emails, photos } = profile;
-
-  //     const user = {
-  //       email: emails[0].value,
-  //       firstname: name?.givenName,
-  //       lastname: name?.familyName,
-  //       photo: photos?.[0].value,
-  //       accessToken,
-  //     };
-
-  //     console.log('Datos de usuario:', user);
-
-  //     done(null, user);
-  //   } catch (error) {
-  //     console.error('Error en GoogleStrategy validate:', error);
-  //     done(error, null)
-  //   }
 }
