da1c9c64497bc1e59c5468a5d191ceede054af7c
Cin Noguera <ciin.noguera@hotmail.com>
2024-12-17 10:42:50 -0300

    Google, otra vez

--- src/mp/mp.service.ts
+++ src/mp/mp.service.ts
@@ -1,12 +1,12 @@
-// import { Injectable } from '@nestjs/common';
-// import * as MercadoPago from 'mercadopago';
-// import { mercadoPagoConfig } from '../config/mpConfig';
-// import { ProductService } from 'src/product/product.service';
+import { Injectable } from '@nestjs/common';
+import * as MercadoPago from 'mercadopago';
+import { mercadoPagoConfig } from '../config/mpConfig';
+import { ProductService } from 'src/product/product.service';
 
 
-// @Injectable()
-// export class MpService {
-//     constructor(
-//         private productService: ProductService,
-//     ){}
-// }
+@Injectable()
+export class MpService {
+    constructor(
+        private productService: ProductService,
+    ){}
+}
