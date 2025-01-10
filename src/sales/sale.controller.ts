4fb65e9c2e69a311185a77c4eabd811ea26babcb
Cin Noguera <ciin.noguera@hotmail.com>
2025-01-10 14:27:02 -0300

    Arreglo RoleGuard

--- src/sales/sale.controller.ts
+++ src/sales/sale.controller.ts
@@ -1,18 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { SaleService } from './sale.service';
 import { ApiOperation, ApiProperty } from '@nestjs/swagger';
+import { Roles } from 'src/utils/Role/role.decorator';
+import { Role } from 'src/utils/Role/role.enum';
+import { AuthGuard } from 'src/guards/auth.guard';
+import { RoleGuard } from 'src/guards/role.guard';
 
 @Controller('sale')
 export class SaleController {
     constructor(private readonly saleService: SaleService) {}
 
     @ApiOperation({ summary: 'Total Sale By Month'})
+    @Roles(Role.Admin)
+    @UseGuards(AuthGuard, RoleGuard)
     @Get('by-month')
     async getTotalByMonth(){
         return await this.saleService.getTotalByMonth()
     }
 
     @ApiOperation({ summary: 'Total Sale'})
+    @Roles(Role.Admin)
+    @UseGuards(AuthGuard, RoleGuard)
     @Get('total')
     async getTotal(){
             return await this.saleService.getTotal()
