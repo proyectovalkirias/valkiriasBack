087f1c61b51f8a21525f67845dfc29036205b9e7
Cin Noguera <ciin.noguera@hotmail.com>
2025-01-14 16:21:54 -0300

    Login Google

--- src/dtos/createOrderDto.ts
+++ src/dtos/createOrderDto.ts
@@ -13,13 +13,17 @@ export class CreateOrderDto {
   @ApiProperty({
     description: 'Products',
     example: [
-      { id: '550e8400-e29b-41d4-a716-446655440000', size: 'M' },
-      { id: '550e8400-e29b-41d4-a716-446655440001', size: 'L' },
+      { id: '550e8400-e29b-41d4-a716-446655440000', size: 'M', quantity: 2 },
+      { id: '550e8400-e29b-41d4-a716-446655440001', size: 'L', quantity: 1 },
     ],
   })
   @IsArray()
   products: { id: string; size: string, quantity: number }[];
 
+  @ApiProperty({
+    description: 'addressId',
+    example: '550e8400-e29b-41d4-a716-446655440000'
+  })
   @IsString()
   @IsNotEmpty()
   addressId: string;
